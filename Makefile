63e335f7d1ac9945ea8c4ad99f8c0f592c99165a
kroovysteph <kroovy@web.de>
2015-11-09 20:31:56 +0100

    added basic game loop

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 game: main.c
-	gcc -o game main.c -lncurses
+	gcc -o game main.c -lncurses -Wall
--- main.c
+++ main.c
@@ -1,7 +1,42 @@
 #include <ncurses.h>
 
+
+	char ch = 'a';
+	int i = 1;
+
+
+void gameLoop(void);
+bool drawQuit(void);
+
+
+
+
+
+
+
+
+
 int main(void) {
-	
-	
+
+	initscr();
+	cbreak();
+	noecho();
+
+	while (true) {
+		ch = getch();
+
+		if (ch == 'q') {
+			if (true) {      //true durch drawQuit() ersetzen
+				break;
+			}
+		}
+
+
+		mvaddch(i,i, ch);
+		i++;
+	}
+
+	endwin();
+
 	return 0;
 }
